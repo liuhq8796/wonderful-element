3d10161186fb766129732471b94b29e313ae8540
Lucas Liu <100069204+liuhq8796@users.noreply.github.com>
2023-01-09 12:46:42 +0000

    chore: 添加 internal 工作区，将 eslint 配置作为独立依赖迁移到 internal

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,21 +1,5 @@
 module.exports = {
-  env: {
-    browser: true,
-    es2021: true
-  },
   extends: [
-    'plugin:vue/vue3-recommended',
-    'standard-with-typescript'
-  ],
-  overrides: [
-  ],
-  parserOptions: {
-    ecmaVersion: 'latest',
-    sourceType: 'module'
-  },
-  plugins: [
-    'vue'
-  ],
-  rules: {
-  }
+    '@liuhq8796/wonderful-element'
+  ]
 }
