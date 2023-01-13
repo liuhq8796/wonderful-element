24fe837590d47268e38727d7e43d2bacfd1e42b6
Lucas Liu <100069204+liuhq8796@users.noreply.github.com>
2023-01-13 09:37:53 +0000

    feat(components): button 按钮增加根据 type 展示不同颜色的功能

--- packages/components/button/src/button.vue
+++ packages/components/button/src/button.vue
@@ -1,10 +1,17 @@
 <template>
-  <button class="we-button">
+  <button
+    :class="[
+      ns.b(),
+      ns.m(type)
+    ]"
+  >
     <slot />
   </button>
 </template>
 
 <script lang="ts">
+import { useNamespace } from '@liuhq8796/wonderful-element-hooks'
+
 export default {
   name: 'WeButton',
 }
@@ -20,33 +27,84 @@ export interface ButtonProps {
 withDefaults(defineProps<ButtonProps>(), {
   type: '',
 })
+
+const ns = useNamespace('button')
 </script>
 
-<style>
+<style lang="scss">
+$input-height: 32px;
+
 .we-button {
     display: inline-flex;
     justify-content: center;
     align-items: center;
 
     line-height: 1;
-    height: 32px;
+    height: $input-height;
     white-space: nowrap;
     cursor: pointer;
-    color: #606266;
+    color: var(--we-button-text-color);
     text-align: center;
     box-sizing: border-box;
     outline: none;
     transition: 0.1s;
-    font-weight: 500;
+    font-weight: var(--we-button-font-weight);
     user-select: none;
     vertical-align: middle;
     -webkit-appearance: none;
     appearance: none;
-    background-color: #ffffff;
-    border: 1px solid #dcdfe6;
-    border-color: #dcdfe6;
+    background-color: var(--we-button-bg-color);
+    border: var(--we-border);
+    border-color: var(--we-button-border-color);
     padding: 8px 15px;
     font-size: 14px;
     border-radius: 4px;
+
+    & + & {
+      margin-left: 12px;
+    }
+}
+
+.we-button {
+  --we-button-text-color: var(--we-text-color-regular);
+  --we-button-font-weight: 500;
+  --we-button-bg-color: var(--we-color-white);
+  --we-button-border-color: #dcdfe6;
+
+  &--primary {
+    --we-button-bg-color: var(--we-color-primary);
+    --we-button-text-color: var(--we-text-color-white)
+  }
+  &--success {
+    --we-button-bg-color: var(--we-color-success);
+    --we-button-text-color: var(--we-text-color-white)
+  }
+  &--info {
+    --we-button-bg-color: var(--we-color-info);
+    --we-button-text-color: var(--we-text-color-white)
+  }
+  &--warning {
+    --we-button-bg-color: var(--we-color-warning);
+    --we-button-text-color: var(--we-text-color-white)
+  }
+  &--danger {
+    --we-button-bg-color: var(--we-color-danger);
+    --we-button-text-color: var(--we-text-color-white)
+  }
+}
+
+:root {
+  // Color
+  --we-color-white: #ffffff;
+  --we-color-black: #000000;
+  --we-color-primary: #409eff;
+  --we-color-success: #67c23a;
+  --we-color-warning: #e6a23c;
+  --we-color-danger: #f56c6c;
+  --we-color-error: #f56c6c;
+  --we-color-info: #909399;
+  --we-border: 1px solid #dcdfe6;
+  --we-text-color-regular: #606266;
+  --we-text-color-white: #ffffff;
 }
 </style>
