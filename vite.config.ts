68018f0e17adc0693017ead41108ac6f40a2df5e
Lucas Liu <100069204+liuhq8796@users.noreply.github.com>
2023-05-24 16:06:22 +0000

    chore: 安装 unplugin-vue-components

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-vue-components/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -13,7 +14,8 @@ export default defineConfig({
     AutoImport({
       // global imports to register
       imports: ['vue', 'vue-router', 'pinia']
-    })
+    }),
+    Components({})
   ],
   resolve: {
     alias: {
